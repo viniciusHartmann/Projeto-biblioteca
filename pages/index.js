f1d8b7704604f95dec186b36e69f35ca76223d8e
Vinicius Hartmann <91153595+viniciusHartmann@users.noreply.github.com>
2024-06-15 22:30:04 -0300

    fix: iniciando botão de menu

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,46 @@
-function Home() {
-    return <div>VINICIOO</div>
-}
+// src/MenuClick.js
+import React, { useState } from 'react';
+import { Menu } from 'devextreme-react/menu';
+import 'devextreme/dist/css/dx.light.css';
 
-export default Home
+const Opcoes = [
+  { text: 'Tela 1', path: '/tela1' },
+  { text: 'Tela 2', path: '/tela2' },
+  { text: 'Tela 3', path: '/tela3' }
+];
+
+const MenuClick = ({ history }) => {
+  const [isMenuVisivel, setMenuVisivel] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisivel(!isMenuVisivel);
+  };
+
+  const onItemClick = (e) => {
+    const item = e.itemData;
+    if (item.path) {
+      history.push(item.path);
+      setMenuVisivel(false);
+    }
+  };
+
+  return (
+    <div>
+      <img
+        src="/images/retangulos-de-menu.png"
+        onClick={toggleMenu}
+        style={{ cursor: 'pointer' }}
+      />
+      {isMenuVisivel && (
+        <Menu
+          dataSource={Opcoes}
+          displayExpr="text"
+          onItemClick={onItemClick}
+          orientation='vertical'
+        />
+      )}
+    </div>
+  );
+};
+
+export default MenuClick;
